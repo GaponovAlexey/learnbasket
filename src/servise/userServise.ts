4529cdfe891dd125add50565e7175f63111f5b0f
kekelberi <alexei.gaponov@gmail.com>
2021-12-28 20:39:39 +0300

    save

--- src/servise/userServise.ts
+++ src/servise/userServise.ts
@@ -1,28 +1,35 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-const userAPI = 'https://jsonplaceholder.typicode.com'
+const userAPI = 'https://jsonplaceholder.typicode.com/'
 
 export interface IPost {
-  id: number;
-  title: string;
+  id: number
+  title: string
   body: string
 }
 
+// export const PostsServise = createApi({
+//   reducerPath: 'PostsServise',
+//   baseQuery: fetchBaseQuery({ baseUrl: userAPI }),
+//   endpoints: (builder) => ({
+//     fetchAllPosts: builder.query<IPost[], number>({
+//       query: (limit: 5) => ({
+//         url: `/posts`,
+//         params: {
+//           _limit: limit
+//         }
+//       }),
+//     }),
+//   }),
+// })
 
 export const PostsServise = createApi({
-  reducerPath: 'PostsServise',
+  reducerPath: 'mypath',
   baseQuery: fetchBaseQuery({ baseUrl: userAPI }),
-  endpoints: (builder) => ({
-    fetchAllPosts: builder.query<IPost[], number>({
-      query: (limit: 5) => ({
-        url: `/posts`,
-        params: {
-          _limit: limit
-        }
+  endpoints: (build) => ({
+    fetchAllPosts: build.query<IPost[], number>({
+      query: () => ({
+        url: `posts`,
       }),
     }),
   }),
 })
-
-// Export hooks for usage in functional components, which are
-// auto-generated based on the defined endpoints
-// export const { useGetPokemonByNameQuery } = PostsServise
